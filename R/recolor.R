e77c0cb2c33d763a0e94dff44725e3c16fd7885b
jmrussell <jonathon.m.russell@gmail.com>
2020-04-23 12:56:30 -0500

    Update recolor.R

--- R/recolor.R
+++ R/recolor.R
@@ -54,12 +54,12 @@ recolor_s <- function(mapRes_sub, obj, output, color = NULL)
     png(paste0(output, '.recolor.tsne.png'))
 		DimPlot(obj, label = T, label.size = 8, group.by = 'regroup',
 			reduction = "tsne",
-			colors.use = color[sort(as.numeric(unique(new_group)))], plot.title = toupper(output))
+			cols = color[sort(as.numeric(unique(new_group)))], plot.title = toupper(output))
     dev.off()
 	pdf(paste0(output, '.recolor.tsne.pdf'))
 		DimPlot(obj, label = T, label.size = 8, group.by = 'regroup',
 			reduction = "tsne",
-			colors.use = color[sort(as.numeric(unique(new_group)))], plot.title = toupper(output))
+			cols = color[sort(as.numeric(unique(new_group)))], plot.title = toupper(output))
     dev.off()
     return(new_group)
 }	   
